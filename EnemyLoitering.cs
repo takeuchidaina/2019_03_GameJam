48809dc27a87ed6def6932578ea0ca0046d6b42a
tajima <tajima.a.work@gmail.com>
2019-03-19 17:23:12 +0900

    [update]徘徊敵の移動処理完了

--- EnemyLoitering.cs
+++ EnemyLoitering.cs
@@ -4,39 +4,48 @@ using UnityEngine;
 
 public class EnemyLoitering : MonoBehaviour
 {
-
-    int[] moveArray = new int[18] { 2, 2, 2, 2, 4, 4, 4, 4, 4, 8, 8, 8, 8, 6, 6, 6, 6, 6 };
+    int[] moveArray = new int[38] {//テンキー方向と一緒のベクトル
+        2, 2, 2, 2, 2, 2, 2, 2,
+        4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
+        8, 8, 8, 8, 8, 8, 8, 8,
+        6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6
+    };
     int cnt;
-    int cntcnt;
+    int cntcnt;//cntを1増やす用のcnt
     float speed;
     private Vector3 vector;
 
     // Start is called before the first frame update
     void Start()
     {
-        int[] moveArray = new int[18] { 2, 2, 2, 2, 4, 4, 4, 4, 4, 8, 8, 8, 8, 6, 6, 6, 6, 6 };
+        int[] moveArray = new int[38] {
+        2, 2, 2, 2, 2, 2, 2, 2,
+        4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
+        8, 8, 8, 8, 8, 8, 8, 8,
+        6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6
+    };
         /*int cnt = 0;
         int cntcnt = 0;
         float speed = 0.05f;*/
-        Vector3 vector= new Vector3(0f,0f,0f);
+        Vector3 vector = new Vector3(0f,0f,0f);
     }
 
 // Update is called once per frame
 void Update()
     {
         cntcnt++;
-        if (cntcnt >= 20)
+        if (cntcnt >= 9)
         {
             cntcnt = 0;
             cnt++;
         }
-        if (cnt > 17)
+        if (cnt >= 38)
         {
             cnt = 0;
         }
 
         Debug.Log(cnt);
-        moveEnemy(moveArray[cnt]);
+        moveEnemy(moveArray[cnt]);//移動処理
     }
 
     void moveEnemy(int num)
