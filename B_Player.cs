7a0e640fc5911539749d6e139b67686c39de4d47
takeuchidaina <yda.takeuchidaina@gmail.com>
2019-03-19 16:51:57 +0900

    [update]プレイヤーにバフが追加
    当たるとステータスアップ

--- B_Player.cs
+++ B_Player.cs
@@ -9,6 +9,10 @@ public class B_Player : MonoBehaviour
     private float move_x;
     private float move_y;
     private float speed;
+    private bool playerHit_up;
+    private bool playerHit_right;
+    private bool playerHit_down;
+    private bool playerHit_left;
 
     // Start is called before the first frame update
     void Start()
@@ -18,6 +22,11 @@ public class B_Player : MonoBehaviour
         move_x = 0.0f;
         move_y = 0.0f;
         speed = 0.02f;
+
+        playerHit_up = false;
+        playerHit_right = false;
+        playerHit_down = false;
+        playerHit_left = false;
     }
 
     // Update is called once per frame
@@ -25,33 +34,68 @@ public class B_Player : MonoBehaviour
     {
         move_x = 0;
         move_y = 0;
+
         PlayerMove();
+
+        PlayerHit();
     }
 
     void PlayerMove()
     {
         if (Input.GetKey(KeyCode.UpArrow))
         {
-            Debug.Log("上");
             move_y = speed;
         }
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            Debug.Log("右");
             move_x = speed;
         }
         if (Input.GetKey(KeyCode.DownArrow))
         {
-            Debug.Log("下");
             move_y = -speed;
         }
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            Debug.Log("左");
             move_x = -speed;
         }
 
         transform.position += new Vector3(move_x, move_y, 0);
 
+    }
+
+    //プレイヤー同士の当たり判定
+    void PlayerHit()
+    {
+        playerHit_up = Physics2D.Raycast(
+     transform.position, Vector2.up,
+     0.5f, 1 << LayerMask.NameToLayer("B_Player"));
+
+        playerHit_right = Physics2D.Raycast(
+    transform.position, Vector2.right,
+    0.5f, 1 << LayerMask.NameToLayer("B_Player"));
+
+        playerHit_down = Physics2D.Raycast(
+    transform.position, Vector2.down,
+    0.5f, 1 << LayerMask.NameToLayer("B_Player"));
+
+        playerHit_left = Physics2D.Raycast(
+    transform.position, Vector2.left,
+    0.5f, 1 << LayerMask.NameToLayer("B_Player"));
+
+        if (playerHit_up || playerHit_right || playerHit_down || playerHit_left)
+        {
+            Debug.Log("当たっている");
+            PlayerStatusUp();
+        }
+        else
+        {
+            Debug.Log("当たってない");
+        }
+    }
+
+    //バフ
+    void PlayerStatusUp()
+    {
+        speed = 0.05f;
     }
 }
--- R_Player.cs
+++ R_Player.cs
@@ -9,6 +9,10 @@ public class R_Player : MonoBehaviour
     private float move_x;
     private float move_y;
     private float speed;
+    private bool playerHit_up;
+    private bool playerHit_right;
+    private bool playerHit_down;
+    private bool playerHit_left;
 
     // Start is called before the first frame update
     void Start()
@@ -18,16 +22,26 @@ public class R_Player : MonoBehaviour
         move_x = 0.0f;
         move_y = 0.0f;
         speed = 0.02f;
+
+        playerHit_up = false;
+        playerHit_right = false;
+        playerHit_down = false;
+        playerHit_left = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        move_x = 0;
-        move_y = 0;
+        move_x = 0.0f;
+        move_y = 0.0f;
+
         PlayerMove();
+
+        PlayerHit();
+
     }
 
+    //十字移動
     void PlayerMove()
     {
         if (Input.GetKey(KeyCode.W))
@@ -50,4 +64,42 @@ public class R_Player : MonoBehaviour
         transform.position += new Vector3(move_x, move_y, 0);
 
     }
+
+    //プレイヤー同士の当たり判定
+    void PlayerHit()
+    {
+        playerHit_up = Physics2D.Raycast(
+     transform.position, Vector2.up,
+     0.5f, 1 << LayerMask.NameToLayer("B_Player"));
+
+        playerHit_right = Physics2D.Raycast(
+    transform.position, Vector2.right,
+    0.5f, 1 << LayerMask.NameToLayer("B_Player"));
+
+        playerHit_down = Physics2D.Raycast(
+    transform.position, Vector2.down,
+    0.5f, 1 << LayerMask.NameToLayer("B_Player"));
+
+        playerHit_left = Physics2D.Raycast(
+    transform.position, Vector2.left,
+    0.5f, 1 << LayerMask.NameToLayer("B_Player"));
+
+        if (playerHit_up || playerHit_right || playerHit_down || playerHit_left)
+        {
+            Debug.Log("当たっている");
+            PlayerStatusUp();
+        }
+        else
+        {
+            Debug.Log("当たってない");
+        }
+    }
+
+    //バフ
+    void PlayerStatusUp()
+    {
+        speed = 0.05f;
+    }
+
+
 }
