19792b279a797534332cd05930797d7145813db8
tajima <tajima.a.work@gmail.com>
2019-03-20 14:47:52 +0900

    [add]タイトルと操作説明の追加

---
+++ Script/B_Title.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class B_Title : MonoBehaviour
+{
+    public GameObject R_Player;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // transformを取得
+        Transform myTransform = this.transform;
+
+        // 座標を取得
+        Vector3 pos = myTransform.position;
+
+        pos.x += 0.01f;    // x座標へ0.01加算
+
+        if (pos.x >= 7.0)
+        {
+            pos.x = -7;
+            myTransform.position = pos;
+
+            Change();
+        }
+
+        myTransform.position = pos;  // 座標を設定
+    }
+    public void Change()
+    {
+        Instantiate(R_Player, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+}
+
--- Script/Button.cs
+++ Script/Button.cs
@@ -19,12 +19,34 @@ public class Button : MonoBehaviour
         SceneManager.LoadScene("Manual");
     }
 
-    //Backボタンを押したとき
+    //Titleボタンを押したとき
     //Titleシーンに移動
-    public void BackButton()
+    public void TitleButton()
     {
         SceneManager.LoadScene("Title");
     }
 
+    //NEXTボタン
+    //次のシーンに移動
+    public void NextButton()
+    {
+        SceneManager.LoadScene("Manual_2");
+    }
 
+    public void NextButton_2()
+    {
+        SceneManager.LoadScene("Manual_3");
+    }
+
+    //BACKボタン
+    //前のシーンに移動
+    public void BackButton()
+    {
+        SceneManager.LoadScene("Manual");
+    }
+
+    public void BackButton_2()
+    {
+        SceneManager.LoadScene("Manual_2");
+    }
 }
