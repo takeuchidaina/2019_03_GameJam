a68b53e38e4d51464cfc374ef8e0637c105de88c
takeuchidaina <yda.takeuchidaina@gmail.com>
2019-03-20 11:04:37 +0900

    [update]ゴールの追加
    二人のプレイヤーが入るとゴールします

---
+++ Goal.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Goal : MonoBehaviour
+{
+    private bool R_PlayerInGoal;
+    private bool B_PlayerInGoal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        R_PlayerInGoal = false;
+        B_PlayerInGoal = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(R_PlayerInGoal == true && B_PlayerInGoal == true)
+        {
+            Debug.Log("ゲームクリア―");
+            //SceneManager.LoadScene("GameClear");
+        }
+    }
+
+    //両方がゴールしたら
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (LayerMask.LayerToName(other.gameObject.layer) == "R_Player")
+        {
+            R_PlayerInGoal = true;
+        }
+        if (LayerMask.LayerToName(other.gameObject.layer) == "B_Player")
+        {
+            B_PlayerInGoal = true;
+        }
+    }
+}
